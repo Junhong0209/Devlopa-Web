e3c34293553b7330e6a256b4cff36fa888d56ce6
Junhong0209 <junh040209@naver.com>
2022-04-12 10:25:05 +0900

    :sparkles: :: 게시글 작성 페이지 추가

---
+++ src/pages/apply.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+import Header from "src/components/Header";
+import ApplyComponent from "src/components/Apply/index";
+
+const Apply: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <ApplyComponent />
+    </>
+  );
+};
+
+export default Apply;
