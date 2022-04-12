6dd365af73880cb4fbcb924fd55c8c280f6eb400
Junhong0209 <junh040209@naver.com>
2022-04-12 14:53:14 +0900

    :sparkles: :: Edit, Delete 이미지 추가

--- src/components/Dashboard/PostItem/index.tsx
+++ src/components/Dashboard/PostItem/index.tsx
@@ -1,6 +1,9 @@
 import dynamic from 'next/dynamic';
+import Image from 'next/image';
 import * as S from 'src/components/Dashboard/PostItem/index.style';
 import User from './User';
+import Edit from 'public/image/Edit.svg';
+import Delete from 'public/image/Delete.svg';
 
 const Viewer = dynamic(
   () => import('src/components/Dashboard/PostViewer/index'),
@@ -12,7 +15,11 @@ const PostItem = () => {
 
   return (
     <S.Container>
-      <User username='박준홍' date='2022. 04. 07' />
+        <User username='박준홍' date='2022. 04. 07' />
+        <S.ImageGroup>
+          <Image src={Edit} width='25' height='25' alt='수정 이미지' />
+          <Image src={Delete} width='25' height='25' alt='삭제 이미지' />
+        </S.ImageGroup>
       <Viewer text={text} />
     </S.Container>
   );
