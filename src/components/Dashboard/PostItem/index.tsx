4be63060acc22b64a838b653d76bd4b8773d9106
Junhong0209 <junh040209@naver.com>
2022-04-14 11:51:32 +0900

    :art: :: Image onClick Event 추가

--- src/components/Dashboard/PostItem/index.tsx
+++ src/components/Dashboard/PostItem/index.tsx
@@ -11,14 +11,14 @@ const Viewer = dynamic(
 );
 
 const PostItem = () => {
-  const text: string = 'asdfasfas \n # hi \n * [x] asdf \n * [x] asdf \n * [x] asdf \n * [x] asdf \n * [x] asdf';
+  const text: string = '# hi \n * [x] asdf \n * [x] asdf \n * [x] asdf \n * [x] asdf \n * [x] asdf';
 
   return (
     <S.Container>
         <User username='박준홍' date='2022. 04. 07' />
         <S.ImageGroup>
-          <Image src={Edit} width='25' height='25' alt='수정 이미지' />
-          <Image src={Delete} width='25' height='25' alt='삭제 이미지' />
+          <Image src={Edit} width='25' height='25' alt='수정 이미지' onClick={() => {alert('수정 버튼 클릭!')}} />
+          <Image src={Delete} width='25' height='25' alt='삭제 이미지' onClick={() => {alert('삭제 버튼 클릭!')}} />
         </S.ImageGroup>
       <Viewer text={text} />
     </S.Container>
