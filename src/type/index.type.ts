7925e43378fb11a663ccd784a253fba30b9b3d67
Junhong0209 <junh040209@naver.com>
2022-04-19 10:08:36 +0900

    :sparkles: :: Type 추가

---
+++ src/type/index.type.ts
@@ -0,0 +1,10 @@
+export interface IPostingData {
+  idx: number;
+  user_name: string;
+  profile_image: string | null;
+  grade: number;
+  room: number;
+  number: number;
+  write_time: string;
+  content: string;
+}
