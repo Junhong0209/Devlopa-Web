2a486392dc2ce14d3cb1a3f3c0db55d9ad7e69dc
Junhong0209 <junh040209@naver.com>
2022-04-19 10:09:46 +0900

    :art: :: 코드 format 재설정

--- src/lib/api/index.api.ts
+++ src/lib/api/index.api.ts
@@ -1,8 +1,8 @@
 import { request } from "../Axios";
 
 export const handleGetUrl = async () => {
-  const url: string = 'v1/url/';
-  const { data } = await request.get(url);
+  const url: string = "v1/url/";
+  const { data }: any = await request.get(url);
 
   return data;
-}
+};
