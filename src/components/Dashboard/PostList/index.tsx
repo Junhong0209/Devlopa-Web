2038087ebe062cbeddd30294780ae5465099878d
Junhong0209 <junh040209@naver.com>
2022-04-19 14:32:40 +0900

    :fire: :: postlist component에서 api 요청 제거

--- src/components/Dashboard/PostList/index.tsx
+++ src/components/Dashboard/PostList/index.tsx
@@ -1,33 +1,14 @@
-import PostItem from '../PostItem';
-import { IPostingData } from 'src/type/index.type';
+import PostItem from "../PostItem";
 
-import * as S from 'src/components/Dashboard/PostList/index.style';
-import { useEffect, useState } from 'react';
-import { handelGetPost } from 'src/lib/api/post.api';
-import { NextRouter, useRouter } from 'next/router';
+import * as S from "src/components/Dashboard/PostList/index.style";
 
-const PostList = () => {
-  const router: NextRouter = useRouter();
-  const [postDatas, setPostdatas] = useState<IPostingData[]>();
-  const code: string | string[] | undefined = router.query.code;
-  
-  useEffect(() => {
-    if (!code) {
-      handelGetPost().then(res => {
-        setPostdatas(res.data.contents);
-      }).catch(err => {
-        console.log(err);
-      });
-    }
-  }, [code]);
-
+const PostList = ({ postDatas }: any) => {
   return (
     <S.Container>
-      {
-        postDatas && postDatas.map((postData: IPostingData) => {
-          return <PostItem key={postData.idx} data={postData} />
-        })
-      }
+      {postDatas &&
+        postDatas.map((postData: any) => {
+          return <PostItem key={postData.idx} data={postData} />;
+        })}
     </S.Container>
   );
 };
