8abb9bc0661dae3b4ec9be6cf3d53fb674752f58
Junhong0209 <junh040209@naver.com>
2022-04-19 14:23:38 +0900

    :sparkles: :: 유저 프로필 가져오는 함수 생성

--- src/lib/api/auth.api.ts
+++ src/lib/api/auth.api.ts
@@ -1,10 +1,17 @@
-import { request } from "../Axios";
+import { instance, request } from "../Axios";
 
 export const handleGetDodamUser = async (
   req: string | string[] | undefined
 ) => {
   const url: string = "v1/auth/";
   const { data }: any = await request.post(url, { code: req });
+
+  return data;
+};
+
+export const handleGetUserProfile = async () => {
+  const url: string = "v1/profile/";
+  const { data }: any = await instance.get(url);
 
   return data;
 };
