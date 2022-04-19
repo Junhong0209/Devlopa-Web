4eaeb6fda2b65f91214de051e872cd581d2556dc
Junhong0209 <junh040209@naver.com>
2022-04-19 10:05:18 +0900

    :art: :: header 값 변경

--- src/utils/refresh.ts
+++ src/utils/refresh.ts
@@ -1,10 +1,10 @@
 import { AxiosRequestConfig } from "axios";
 
 export const refresh = async (config: any): Promise<AxiosRequestConfig> => {
-  let accessToken = sessionStorage.getItem('access_token');
+  let accessToken = sessionStorage.getItem("access_token");
 
-  config.headers["Access-Control-Allow-Origin"] = '*';
-  config.headers["Authorization"] = `Bearer ${accessToken}`;
+  config.headers["Access-Control-Allow-Origin"] = "*";
+  config.headers["Authorization"] = `Token ${accessToken}`;
 
   return config;
 };
