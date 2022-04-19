b01ae314d5367da14020084ba409bce7b9642479
Junhong0209 <junh040209@naver.com>
2022-04-19 14:31:24 +0900

    :sparkles: :: Dashboard 화면으로 이동하면 username을 저장하여 header에 보여줌

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,39 @@
-import { useRouter } from 'next/router';
-import type { NextRouter } from 'next/router';
+import { Username } from "src/store";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+import type { NextRouter } from "next/router";
 
-import Image from 'next/image';
-import MainLogo from 'public/image/Main_Logo.svg';
-import UserDefault from 'public/image/Default_User_Icon.svg';
+import Image from "next/image";
+import MainLogo from "public/image/Main_Logo.svg";
+import UserDefault from "public/image/Default_User_Icon.svg";
 
-import * as S from 'src/components/Header/index.style';
+import * as S from "src/components/Header/index.style";
 
 const Header = () => {
   const router: NextRouter = useRouter();
+  const username = useRecoilValue(Username);
 
   return (
     <S.Container>
       <S.Layout>
         <S.Image>
-          <Image src={MainLogo} width='185px' height='40px' alt='Main Logo' onClick={() => router.push('/dashboard')} />
+          <Image
+            src={MainLogo}
+            width="185px"
+            height="40px"
+            alt="Main Logo"
+            onClick={() => router.push("/dashboard")}
+          />
         </S.Image>
-        <S.User onClick={() => router.push('/user')}>
-          <S.UserName>
-            박준홍
-          </S.UserName>
+        <S.User onClick={() => router.push("/user")}>
+          <S.UserName>{username}</S.UserName>
           <S.UserImage>
-            <Image src={UserDefault} width='36px' height='36px' alt='Default Icon' />
+            <Image
+              src={UserDefault}
+              width="36px"
+              height="36px"
+              alt="Default Icon"
+            />
           </S.UserImage>
         </S.User>
       </S.Layout>
---
+++ src/store/index.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const Username = atom({
+  default: "",
+  key: "getUserName",
+});
