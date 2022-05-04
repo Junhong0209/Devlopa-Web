a9d4d8d094603b1d7705774b49ecd9ed41270705
Junhong0209 <junh040209@naver.com>
2022-05-04 11:38:31 +0900

    :ambulance: :: loading and error handling

--- src/components/Modify/index.style.ts
+++ src/components/Modify/index.style.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding-top: 60px;
   width: 62.5%;
+  height: 100%;
   margin: auto;
 `;
 
@@ -10,4 +11,31 @@ export const Title = styled.div`
   margin-top: 80px;
   margin-bottom: 80px;
   font-size: 40px;
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  height: 100%;
+  vertical-align: middle;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  & > .loading {
+    margin-right: 3rem;
+  }
+`;
+
+export const LoadingText = styled.div`
+  display: flex;
+  font-size: 24px;
+  text-align: center;
+`;
+
+export const Error = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: -50px;
+  font-size: 24px;
 `;
--- src/components/Modify/index.tsx
+++ src/components/Modify/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import ReactLoading from "react-loading";
 import { NextRouter, useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { handleGetPost } from "src/api/post.api";
@@ -11,28 +12,43 @@ const Editor = dynamic(() => import("src/components/Editor/index"), {
 
 const ModifyContainer = (): JSX.Element => {
   const [initialValue, setInitialValue] = useState<string>();
+  const [loading, isLoading] = useState<boolean>(false);
+  const [error, isError] = useState<boolean>(false);
   const router: NextRouter = useRouter();
 
   useEffect(() => {
+    isLoading(true);
     handleGetPost({ post_idx: router.query["id"] })
       .then((res) => {
-        console.log(res.data.contents.content);
         setInitialValue(res.data.contents.content);
+        isLoading(false);
       })
       .catch((err) => {
-        console.log(err);
+        isError(true);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [router]);
 
   return (
     <S.Container>
-      <S.Title>게시글 수정</S.Title>
-      <Editor
-        idx={router.query["id"]}
-        initialValue={initialValue}
-        buttonText="수정완료"
-      />
+      {loading ? (
+        error ? (
+          <S.Error>게시글 정보를 가지고 오지 못했습니다.</S.Error>
+        ) : (
+          <S.Loading>
+            <ReactLoading type="spin" color="#36383C" className="loading" />
+            <S.LoadingText>게시글 정보 가져오는 중...</S.LoadingText>
+          </S.Loading>
+        )
+      ) : (
+        <>
+          <S.Title>게시글 수정</S.Title>
+          <Editor
+            idx={router.query["id"]}
+            initialValue={initialValue}
+            buttonText="수정완료"
+          />
+        </>
+      )}
     </S.Container>
   );
 };
