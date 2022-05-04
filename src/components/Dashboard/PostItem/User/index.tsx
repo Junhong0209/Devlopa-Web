f0d96f615909a67af59ae17cd09c64b255d3fb51
Junhong0209 <junh040209@naver.com>
2022-05-04 09:35:28 +0900

    :art: :: user title click
    유저 프로필 또는 유저 이름 클릭 시 onClick Event 추가

--- src/components/Dashboard/PostItem/User/index.tsx
+++ src/components/Dashboard/PostItem/User/index.tsx
@@ -2,13 +2,32 @@
 import Image from "next/image";
 import UserImage from "public/image/Default_User_Icon.svg";
 import * as S from "src/components/Dashboard/PostItem/User/index.style";
-import { IUserProps } from "src/type/user.type";
 
-const User = ({ username, date }: IUserProps): JSX.Element => {
+interface Props {
+  username: string;
+  date: string;
+}
+
+const User = ({ username, date }: Props): JSX.Element => {
   return (
     <S.Container>
-      <Image src={UserImage} width={36} height={36} />
-      <S.Name>{username}</S.Name>
+      <S.ImageWrapper>
+        <Image
+          src={UserImage}
+          width={36}
+          height={36}
+          onClick={() => {
+            // Todo
+          }}
+        />
+      </S.ImageWrapper>
+      <S.Name
+        onClick={() => {
+          // Todo
+        }}
+      >
+        {username}
+      </S.Name>
       <S.Time>{date}</S.Time>
     </S.Container>
   );
