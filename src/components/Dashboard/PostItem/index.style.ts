6f9a6bd56325d44e8557c736ae6228c314dd8967
Junhong0209 <junh040209@naver.com>
2022-05-09 11:55:15 +0900

    :lipstick: :: modify padding

--- src/components/Dashboard/PostItem/index.style.ts
+++ src/components/Dashboard/PostItem/index.style.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.1);
+  padding-bottom: 50px;
   margin-bottom: 30px;
   border-radius: 12px;
 `;
--- src/components/Dashboard/PostViewer/index.style.ts
+++ src/components/Dashboard/PostViewer/index.style.ts
@@ -4,5 +4,4 @@ export const Container = styled.div`
   width: 94.3%;
   height: auto;
   margin: auto;
-  padding-bottom: 50px;
 `;
