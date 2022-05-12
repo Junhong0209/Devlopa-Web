577cd516122f6fc8aa7b22929df61e41df853285
Junhong0209 <junh040209@naver.com>
2022-05-12 09:18:27 +0900

    :art: :: Change Alert Code
    alert 코드 수정

--- src/lib/SweetAlert/index.ts
+++ src/lib/SweetAlert/index.ts
@@ -1,7 +1,7 @@
 import type { NextRouter } from "next/router";
 import Swal from "sweetalert2";
 
-interface Props {
+interface AllProps {
   icon: any;
   title: string;
   text: string;
@@ -9,10 +9,44 @@ interface Props {
   url: string | null;
 }
 
-export const CustomAlert = ({ icon, title, text, router, url }: Props) => {
+interface Props {
+  text: string;
+  router: NextRouter | null;
+  url: string | null;
+}
+
+export const CustomAlert = ({ icon, title, text, router, url }: AllProps) => {
   Swal.fire({
     icon: icon,
     title: title,
+    text: text,
+  }).then((result) => {
+    if (result.isConfirmed && router && url) {
+      router.push(url);
+    } else if (result.isConfirmed && router) {
+      router.reload();
+    }
+  });
+};
+
+export const SuccessAlert = ({ text, router, url }: Props) => {
+  Swal.fire({
+    icon: "success",
+    title: "Success!",
+    text: text,
+  }).then((result) => {
+    if (result.isConfirmed && router && url) {
+      router.push(url);
+    } else if (result.isConfirmed && router) {
+      router.reload();
+    }
+  });
+};
+
+export const ErrorAlert = ({ text, router, url }: Props) => {
+  Swal.fire({
+    icon: "error",
+    title: "Error!",
     text: text,
   }).then((result) => {
     if (result.isConfirmed && router && url) {
