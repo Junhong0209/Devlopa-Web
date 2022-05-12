f44e9c7091b9ebbde55c088f943735cca9ef5326
Junhong0209 <junh040209@naver.com>
2022-05-12 09:15:00 +0900

    :fire: :: Remove the not using code

--- src/components/Post/Item/index.tsx
+++ src/components/Post/Item/index.tsx
@@ -1,18 +1,13 @@
 import User from "./User";
-import Swal from "sweetalert2";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import Edit from "public/assets/post/Edit.svg";
 import Delete from "public/assets/post/Delete.svg";
 import CommentList from "../Comment/List";
-import { CustomAlert } from "src/lib/SweetAlert";
-import { ErrorToast, SuccessToast } from "src/lib/Toast";
-import { handleDeletePost } from "src/api/post.api";
 import { NextRouter, useRouter } from "next/router";
+import { deletePost } from "src/utils/deletePost";
 
 import * as S from "src/components/Post/Item/index.style";
-import { useEffect } from "react";
-import { useDeletePost } from "src/hooks/useDeletePost";
 
 const Viewer = dynamic(() => import("src/components/Viewer/index"), {
   ssr: false,
@@ -58,8 +53,7 @@ const PostItem = ({ data, getAllPost, getUserProfile }: Props): JSX.Element => {
               height="25"
               alt="삭제 이미지"
               onClick={() => {
-                // eslint-disable-next-line react-hooks/rules-of-hooks
-                useDeletePost({
+                deletePost({
                   idx: data.idx,
                   getAllPost: getAllPost,
                   getUserProfile: getUserProfile,
