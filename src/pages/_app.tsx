9eb20e8233251f1b1e7588113e9f677b7fde95f2
Junhong0209 <junh040209@naver.com>
2022-05-17 10:19:09 +0900

    :sparkles: :: add themeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,30 @@
-import { RecoilRoot } from "recoil";
-import { Toaster } from "react-hot-toast";
-
-import type { AppProps } from "next/app";
-
 import GlobalStyle from "src/styles/global-styles";
 import ErrorBoundary from "src/components/ErrorBoundary/ErrorBoundary";
 import Modal from "react-modal";
+import { RecoilRoot } from "recoil";
+import { Toaster } from "react-hot-toast";
+import { theme } from "src/styles/theme";
+import { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ErrorBoundary>
-        <GlobalStyle />
-        <Component {...pageProps} />
-        <Toaster
-          position="bottom-center"
-          toastOptions={{
-            style: {
-              width: "560px",
-            },
-          }}
-        />
-      </ErrorBoundary>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <ErrorBoundary>
+          <GlobalStyle />
+          <Component {...pageProps} />
+          <Toaster
+            position="bottom-center"
+            toastOptions={{
+              style: {
+                width: "560px",
+              },
+            }}
+          />
+        </ErrorBoundary>
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/color.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  Black900: 'rgba(0, 0, 0, 0.15)',
+  Black800: 'rgba(18, 24, 33, 0.8)',
+  Black700: 'rgba(16, 18, 23, 0.7)',
+  Black600: '#0f1117',
+  Black500: 'rgba(52, 53, 58, 0.2)',
+
+  Gray900: '#4D4D4D',
+  Gray800: '#C4C4C4',
+  Gray700: '#898FA0',
+
+  Blue900: '#2D67FF',
+  Blue800: '#606AB5',
+  Blue700: 'rgba(96, 109, 146, 0.3)',
+  Blue600: '#606D92',
+  Blue500: '#CCDAFF',
+
+  White: '#FFFFFF',
+
+  Main1: '#101217',
+  Main2: '#36373C',
+}
